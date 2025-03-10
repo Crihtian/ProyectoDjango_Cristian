83c89e9f8b13e3f0c41a7f5312f4ce27304945b9
Cristian López <130441661+Crihtian@users.noreply.github.com>
2025-03-10 21:48:31 +0100

    Redireccion Login

--- ProjectoDesencadenado/urls.py
+++ ProjectoDesencadenado/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/login/', permanent=True)),  # Redirect root URL to login
     path('', include('blog.urls')),  
 ]
 
